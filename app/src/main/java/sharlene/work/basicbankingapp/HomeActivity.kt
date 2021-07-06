7f0ef81950f7f5fb6fb77588970ea7dfd14fdef1
Crackmindedsk <shakumbhar@gmail.com>
2021-07-06 20:43:52 +0530

    HomeActivity

--- app/src/main/java/sharlene/work/basicbankingapp/HomeActivity.kt
+++ app/src/main/java/sharlene/work/basicbankingapp/HomeActivity.kt
@@ -1,6 +1,37 @@
 package sharlene.work.basicbankingapp
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity:AppCompatActivity() {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater:MenuInflater=menuInflater
+        inflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id=item.itemId
+        if (id==R.id.action_history){
+            val intent=Intent(this,TransactionActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+        val ListBtn:Button=findViewById(R.id.start_button)
+        ListBtn.setOnClickListener {
+            val intent=Intent(this@HomeActivity,ViewListActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/sharlene/work/basicbankingapp/MainActivity.kt
+++ app/src/main/java/sharlene/work/basicbankingapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package sharlene.work.basicbankingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val handler= Handler()
+        handler.postDelayed({
+            val homeIntent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(homeIntent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
+    }
+
+    companion object {
+        private const val SPLASH_TIME_OUT=4000
     }
 }
